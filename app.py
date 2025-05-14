09ad52be5f6bbb3fcc0b6495dafe84aef0a7c39e
SyedRazaZawar <75534559+SyedRazaZawar@users.noreply.github.com>
2025-05-14 14:36:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ if chatbot_functionality == "My Chatbot":
 
 # Predefined Gemini API Key
 
-    API_KEY = "AIzaSyD7YOrZrkH4SBkphu50VMJIU2780C7eUQA"  
+    API_KEY = "AIzaSyBS_cEWY4YfNURiEpajC_aKEoqekxc_HTI"  
 
 
 # File to save sessions
